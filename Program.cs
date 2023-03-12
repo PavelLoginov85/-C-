cc5d993a81161882bc5302cea8dbb5b8ce2db9c7
Pavel <pahnologia@yandex.ru>
2023-03-12 21:13:22 +0300

    add decision for second task

--- Program.cs
+++ Program.cs
@@ -1,13 +1,33 @@
 ﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 
-Console.Write("Введите первое число:  ");
-int first = int.Parse(Console.ReadLine()!);
-Console.Write("Введите второе число:  ");
-int second = int.Parse(Console.ReadLine()!);
-if(first > second){
-    Console.Write($"Максимальное число {first}");
-}
+// Console.Write("Введите первое число:  ");
+// int first = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите второе число:  ");
+// int second = int.Parse(Console.ReadLine()!);
+// if(first > second){
+//     Console.Write($"Максимальное число {first}");
+// }
 
-else {
-    Console.Write($"Максимальное число {second}");
+// else {
+//     Console.Write($"Максимальное число {second}");
+// }
+
+
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Write("Введите первое чило: ");
+int num1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе чило: ");
+int num2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите третье чило: ");
+int num3 = int.Parse(Console.ReadLine()!);
+if(num2 < num1 && num1 > num3){
+    Console.Write($"Максимальное число {num1}");
+}
+else if(num2 > num3 && num2 > num1){
+    Console.Write($"Максимальное число {num2}");
+}
+else if(num3 > num2 && num3 > num1){
+    Console.Write($"Максимальное число {num3}");
 }
