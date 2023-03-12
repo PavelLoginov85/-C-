e55ede18cebf5f7c682fef0709d967730cc37a9f
Pavel <pahnologia@yandex.ru>
2023-03-12 21:25:44 +0300

    add decision for fourth 1 task

--- Program.cs
+++ Program.cs
@@ -49,10 +49,10 @@
 //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает 
 //все чётные числа от 1 до N
 
-Console.Write("Введите чило: ");
-int N = int.Parse(Console.ReadLine()!);
-int A = 2;
-while(A<N){
-    Console.Write($"{A} ");
-    A = A+2;
-}
+// Console.Write("Введите чило: ");
+// int N = int.Parse(Console.ReadLine()!);
+// int A = 2;
+// while(A<N){
+//     Console.Write($"{A} ");
+//     A = A+2;
+// }
