0e992ec88ef36ab473491c3de8b96d48410f7b6a
Pavel <pahnologia@yandex.ru>
2023-03-12 21:09:28 +0300

    add decision for first task

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.Write("Введите первое число:  ");
+int first = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число:  ");
+int second = int.Parse(Console.ReadLine()!);
+if(first > second){
+    Console.Write($"Максимальное число {first}");
+}
+
+else {
+    Console.Write($"Максимальное число {second}");
+}
